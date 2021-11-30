c3fbd47dd15b375ab818d754457e1aa54317d2e2
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2021-11-30 18:12:56 +0100

    mods

--- jsnack-1/js/script1.js
+++ jsnack-1/js/script1.js
@@ -1,12 +1,44 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
 
 
-const nameArr = ["Giuseppe", "Emanuele", "Fulvio", "Giando", "Daniele", "Luca" ];
+//creo i miei array
 
-const surnameArr = ["Urbano", "Legend", "Golem", "Fuoco", "Rispetto", "Fratello" ];
+let nameArr = ["Giuseppe", "Emanuele", "Fulvio", "Giando", "Daniele", "Luca" ];
 
-const mergedArray = [...nameArr, ...surnameArr];
-console.log(mergedArray);
+let surnameArr = ["Urbano", "Legend", "Golem", "Fuoco", "Rispetto", "Fratello" ];
+
+//creo l'array degli invitati alla festa 
+
+let guestsArr = [];
+
+let maxValue = 25;
+
+//quindi creo un for che dice che per ogni nome random (variabile che creo nel ciclo), collego un cognome random (variabile che creo nel ciclo)
+
+for (let i = 0; i <= maxValue;) {
+    let numberRandName = getRandNumber (0, nameArr.length);
+
+    let numberRandSurname = getRandNumber (0, surnameArr.length);
 
 
-//??
+    //creo la varbile "invitato" sempre all'interno del ciclo
+
+    let guest = nameArr[numberRandName] + " " + surnameArr[numberRandSurname];
+    
+
+    //controllo se questo invitato è nel mio array
+
+    if (!guestsArr.includes(guest)) {
+        guestsArr.push(guest);
+        i++
+    }
+}
+
+console.log(guestArr);
+
+
+//creo una funzione per unire gli elementi dell'array
+
+function getRandNumber (min, max) {
+    return Math.floor (Math.random() * (max - min)) + min;
+}
